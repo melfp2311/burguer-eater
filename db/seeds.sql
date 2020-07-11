4c918085d271ab4b0295664fb571bf11955bd16f
Melissa Flores <mel.floresp@gmail.com>
2020-07-11 11:21:04 -0500

    adding the db and config folders

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+  
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon and cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', true);
+
